06ce0c6f031e981bd7ce25588c8483056ed461ed
Paul <paul.salina@gmail.com>
2016-10-11 20:38:04 -0400

    add feedback container with remote logic and feedback form

---
+++ src/FeedbackForm.js
@@ -0,0 +1,76 @@
+import React, {Component} from 'react';
+import {FormGroup, FormControl, Button, ControlLabel} from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.css';
+
+export default class FeedbackForm extends Component {
+  constructor(props) {
+    super(props);
+
+    const {review} = props;
+
+    this.state = {
+      feedback: review.feedback ? review.feedback : '',
+      editing: review.feedback === undefined
+    }
+  }
+
+  _feedbackChangeHandler(e) {
+    this.setState(
+      Object.assign(
+        {},
+        this.state,
+        {feedback: e.target.value}
+      )
+    );
+  }
+
+  render() {
+    const {onDone} = this.props;
+    const {grade, notes} = this.props.review;
+    const {feedback, editing} = this.state;
+
+    const onDoneHandler = () => {
+      onDone(feedback);
+      this.setState(Object.assign({}, this.state, {editing: false}))
+    };
+
+    const onEditHandler = () => {
+      this.setState(Object.assign({}, this.state, {editing: true}));
+    };
+
+    return(
+      <div>
+        <h4>
+          {grade}
+        </h4>
+        <p>{notes}</p>
+        {
+          editing ?
+            <div>
+              <FormGroup controlId="feedback" >
+                <FormControl
+                  value={feedback}
+                  componentClass="textarea"
+                  placeholder="What's your feedback?"
+                  onChange={this._feedbackChangeHandler.bind(this)}
+                />
+              </FormGroup>
+              <Button onClick={onDoneHandler}>
+                Submit
+              </Button>
+            </div>
+          :
+          <div>
+
+            <p><span style={{fontWeight: 'bold'}}>your feedback: </span>{feedback}</p>
+            <Button onClick={onEditHandler}>
+              Edit
+            </Button>
+          </div>
+
+        }
+      </div>
+    );
+  }
+
+}
--- src/FeedbackRemoteContainer.js
+++ src/FeedbackRemoteContainer.js
@@ -1,21 +1,98 @@
 import React, { Component } from 'react';
-import { Panel } from 'react-bootstrap';
+import { Panel, ListGroup, ListGroupItem } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.css';
-import './App.css';
+import FeedbackForm from './FeedbackForm';
 
 const DEV_URL = 'http://localhost:1337/parse/classes'
 const EMPLOYEE_URL = DEV_URL + '/employee';
 const REVIEW_URL = DEV_URL + '/review';
 const APPLICATION_ID = 'performanceReview';
 
-// takes the firstName and lastName of the user and checks to see if they
-// exist in the db. If they exist, sets up a redirect to their review page
 export default class FeedbackRemoteContainer extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      reviews: []
+    };
+  }
+
+  componentDidMount() {
+    const {employeeId} = this.props;
+    var myHeaders = new Headers({'X-Parse-Application-Id': APPLICATION_ID});
+
+    var options = {
+      method: 'GET',
+      headers: myHeaders
+    };
+
+    const uri = `${REVIEW_URL}?${encodeURI('where=' + JSON.stringify({employeeId}))}`
+
+    fetch(uri, options)
+      .then((response) => response.json())
+      .then((data) =>
+        this.setState(
+          Object.assign({}, this.state, {reviews: data.results})
+        )
+      )
+  }
+
   render() {
+    const {reviews} = this.state;
     return(
-      <Panel style={{ width: 300 }}>
-        {this.props.employeeId}
+      reviews.length === 0 ?
+
+      <Panel style={{width: 500}}>
+        "No Reviews to give feedback as of now. :)"
       </Panel>
+      :
+      <ListGroup style={{width: 500}}>
+        {
+          reviews.map((review) =>(
+            <ListGroupItem key={review.objectId}>
+              <FeedbackForm
+                review={review}
+                onDone={(feedback) => this._updateFeedback(
+                  review.objectId,
+                  feedback
+                )}
+              />
+            </ListGroupItem>
+          ))
+        }
+      </ListGroup>
     );
+  }
+
+  _updateFeedback(objectId, feedback) {
+    var myHeaders = new Headers({'X-Parse-Application-Id': APPLICATION_ID});
+
+    var options = {
+      method: 'PUT',
+      headers: myHeaders,
+      body: JSON.stringify({
+        feedback
+      })
+    };
+
+    fetch(`${REVIEW_URL}/${objectId}`, options)
+      .then((response) => response.json())
+      .then((data) =>
+        this.setState(
+          Object.assign(
+            {},
+            this.state,
+            {
+              reviews: this.state.reviews
+                .map((review) =>
+                  review.objectId === objectId ?
+                  Object.assign({}, review, {feedback})
+                  :
+                  review
+                )
+            }
+         )
+        )
+      );
   }
 }
